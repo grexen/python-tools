52f71f758387393b613c2e47fdcc86765c45f05a
Greg M <grexen@users.noreply.github.com>
2020-04-06 19:44:03 +0200

    [BUGFIX] Avoid replacing unwanted numbers

--- repace_speed_values.py
+++ repace_speed_values.py
@@ -16,14 +16,18 @@ for filePath in inputFiles:
     listOfLines = fileHandler.readlines()
 
     for line in listOfLines:
+        newLine = line
         regSpeedMatchObject = speedRegex.search(line)
 
         if regSpeedMatchObject and regSpeedMatchObject.group(1):
             speedValueKmh = regSpeedMatchObject.group(1)
             speedValueMs = str(float(speedValueKmh)/3.6)
 
-            line = line.replace(speedValueKmh, speedValueMs)
+            staticPartStart = line[0:regSpeedMatchObject.start(1)]
+            staticPartEnd = line[regSpeedMatchObject.end(1)::]
 
-        outputFile.write(line)
+            newLine = staticPartStart + speedValueMs + staticPartEnd
+
+        outputFile.write(newLine)
 
     fileHandler.close()
